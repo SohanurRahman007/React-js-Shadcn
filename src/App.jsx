834306787199c8989bd77b116948e1c72b94a894
Sohanur Rahman <sohanuractive007@gmail.com>
2026-01-31 12:19:50 +0600

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import React, { useState } from "react";
-import Sidebar from "./components/Sidebar";
-import Topbar from "./components/Topbar";
-import DashboardContent from "./components/DashboardContent";
+import React from "react";
 
 function App() {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
-
   return (
     <div className="min-h-screen bg-gray-50">
-      <Sidebar />
-      <div
-        className={`transition-all duration-300 ${sidebarOpen ? "ml-64" : "ml-0"}`}
-      >
-        <Topbar toggleSidebar={toggleSidebar} />
-        <main className="pt-16 p-6">
-          <DashboardContent />
-        </main>
+      <div className="">
+        <main className="pt-16 p-6">hello react and tailwind+shadcn</main>
       </div>
     </div>
   );
